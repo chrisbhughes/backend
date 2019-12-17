c1813aebb412575d1acf6a925f97390c15f047a3
chrisbhughes <54986773+chrisbhughes@users.noreply.github.com>
2019-12-17 15:37:10 -0500

    Add files via upload

---
+++ api/routes/business.route.js
@@ -0,0 +1,79 @@
+// business.route.js
+
+const express = require('express');
+const app = express();
+const businessRoutes = express.Router();
+
+// Require Business model in our routes module
+let Business = require('../models/Business');
+
+
+// Defined store route
+businessRoutes.route('/add').post(function (req, res) {
+  let business = new Business(req.body);
+  business.save()
+    .then(business => {
+      res.status(200).json({'business': 'Player added successfully'});
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+});
+
+// Defined get data(index or listing) route
+businessRoutes.route('/').get(function (req, res) {
+    Business.find(function (err, businesses){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(businesses);
+    }
+  });
+});
+
+// Defined edit route
+businessRoutes.route('/edit/:id').get(function (req, res) {
+  let id = req.params.id;
+  Business.findById(id, function (err, business){
+      res.json(business);
+  });
+});
+// Defined edit route
+businessRoutes.route('/joingame/:id').get(function (req, res) {
+  let id = req.params.id;
+  Business.findById(id, function (err, business){
+      res.json(business);
+  });
+});
+//  Defined update route
+businessRoutes.route('/update/:id').post(function (req, res) {
+    Business.findById(req.params.id, function(err, business) {
+    if (!business)
+      return next(new Error('Could not load Document'));
+    else {
+        business.person_name = req.body.person_name;
+        business.business_name = req.body.business_name;
+        business.business_gst_number = req.body.business_gst_number;
+        business.player_time = req.body.player_time;
+        business.player_game = req.body.player_game;
+        business.player_ava = req.body.player_ava;
+        business.save().then(business => {
+          res.json('Update complete');
+      })
+      .catch(err => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+businessRoutes.route('/delete/:id').get(function (req, res) {
+    Business.findByIdAndRemove({_id: req.params.id}, function(err, business){
+        if(err) res.json(err);
+        else res.json('Successfully removed');
+    });
+});
+
+module.exports = businessRoutes;
